2680a617f431dbeec9c1ef477f81f74d5aa785c3
Carlos H. <carlos.e.herrera.j@gmail.com>
2025-08-03 14:44:26 -0400

    users
    users

--- users.py
+++ users.py
@@ -9,7 +9,6 @@ class User(BaseModel):
     last_name: str
     age: int
 
-# users: list[User] = [User(name='Nikole', last_name='Herrera', age=10)]
 users: list[User] = []
 users.append(User(id=1, name='Nikole', last_name='Smith', age=10))
 users.append(User(id=2, name='Carlos', last_name='Hernandez', age=44))
@@ -17,30 +16,16 @@ users.append(User(id=3, name='Andres', last_name='Correa', age=25))
 
 @app.get('/users')
 def users_json():
-    # return [
-    #     {'message': 'Hello users', "name": 'Carlos'},
-    #     {'message': 'Hello users', 'name': 'Nikole'},
-    # ]
     return users
 
 @app.get('/users/{id}')
 def user_by_id(id: int):
-    # try:
-    #     users_find = filter(lambda user: user.id == id, users)
-    #     return list(users_find)[0]
-    # except:
-    #     return {'error': 'not found'}
     return search_user_by_id(id=id)
     
 @app.get('/user_query/')
 def user_query(id: int):
-    # try:
-    #     users_find = filter(lambda user: user.id == id, users)
-    #     return list(users_find)[0]
-    # except:
-    #     return {'error': 'not found'}
     return search_user_by_id(id=id)
-    
+
 def search_user_by_id(id: int):
     try:
         users_find = filter(lambda user: user.id == id, users)
