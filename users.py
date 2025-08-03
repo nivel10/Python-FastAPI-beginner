579ca0ed244ec63a7a38818e00297b426677b697
Carlos H. <carlos.e.herrera.j@gmail.com>
2025-08-03 14:11:48 -0400

    users
    users

--- users.py
+++ users.py
@@ -11,8 +11,8 @@ class User(BaseModel):
 
 # users: list[User] = [User(name='Nikole', last_name='Herrera', age=10)]
 users: list[User] = []
-users.append(User(id=1, name='Nikole', last_name='Herrera', age=10))
-users.append(User(id=2, name='Carlos', last_name='Herrera', age=44))
+users.append(User(id=1, name='Nikole', last_name='Smith', age=10))
+users.append(User(id=2, name='Carlos', last_name='Hernandez', age=44))
 users.append(User(id=3, name='Andres', last_name='Correa', age=25))
 
 @app.get('/users')
