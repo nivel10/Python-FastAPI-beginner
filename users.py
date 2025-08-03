9e010be628763a7e1a623a333e75b987425b187c
Carlos H. <carlos.e.herrera.j@gmail.com>
2025-08-03 14:11:12 -0400

    users
    users

---
+++ users.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class User(BaseModel):
+    id: int
+    name: str
+    last_name: str
+    age: int
+
+# users: list[User] = [User(name='Nikole', last_name='Herrera', age=10)]
+users: list[User] = []
+users.append(User(id=1, name='Nikole', last_name='Herrera', age=10))
+users.append(User(id=2, name='Carlos', last_name='Herrera', age=44))
+users.append(User(id=3, name='Andres', last_name='Correa', age=25))
+
+@app.get('/users')
+def users_json():
+    # return [
+    #     {'message': 'Hello users', "name": 'Carlos'},
+    #     {'message': 'Hello users', 'name': 'Nikole'},
+    # ]
+    return users
