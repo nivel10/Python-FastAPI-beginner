14e54a9512570ad5e94085ddaf6740aaa81cab1e
Carlos H. <carlos.e.herrera.j@gmail.com>
2025-08-04 18:53:31 -0400

    products
    products

--- routers/products.py
+++ routers/products.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 from pydantic import BaseModel
 
-router_products = APIRouter()
+router_products = APIRouter(prefix='/products')
 
 # documentation
 ## swagger /docs
@@ -20,16 +20,24 @@ products.append(Product(id=3, description='Product 3', price=254.23))
 products.append(Product(id=4, description='Product 4', price=45.00))
 products.append(Product(id=5, description='Product 5', price=2014.12))
 
-@router_products.get('/products')
+@router_products.get('/')
 async def products_get():
     try:
         return products
     except Exception as ex:
         return {'error': 'proucts get all', 'ex': ex}
 
-@router_products.get('/products/{id}')
+@router_products.get('/{id}')
 async def product_by_id(id: int):
     try:
-        return
+        return products_by_id(id=id)
     except:
-        return
+        return {'error': 'product not found', 'data': id}
+
+# -------------------------------------
+def products_by_id(id=id):
+    try:
+        products_found = filter(lambda product: product.id == id, products)
+        return list(products_found)[0]
+    except:
+        return {'error': 'product not found', 'data': id, }
