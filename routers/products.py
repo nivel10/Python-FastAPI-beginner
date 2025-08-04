0387855d33dc732809918a452cd14dbce2e206a1
Carlos H. <carlos.e.herrera.j@gmail.com>
2025-08-04 18:46:14 -0400

    products
    products

--- routers/products.py
+++ routers/products.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
 
 router_products = APIRouter()
 
@@ -6,10 +7,29 @@ router_products = APIRouter()
 ## swagger /docs
 ## Redoc / redoc
 
+class Product(BaseModel):
+    id: int
+    description: str
+    price: float
+
+# products = list[str] = ['product 1', 'product 2', 'product 3', 'product 4', 'product 5']
+products: list[Product] = []
+products.append(Product(id=1, description='Product 1', price=10.45))
+products.append(Product(id=2, description='Product 2', price=98.87))
+products.append(Product(id=3, description='Product 3', price=254.23))
+products.append(Product(id=4, description='Product 4', price=45.00))
+products.append(Product(id=5, description='Product 5', price=2014.12))
+
 @router_products.get('/products')
 async def products_get():
     try:
-        products: list[str] = ['product 1', 'product 2', 'product 3', 'product 4', 'product 5']
         return products
     except Exception as ex:
         return {'error': 'proucts get all', 'ex': ex}
+
+@router_products.get('/products/{id}')
+async def product_by_id(id: int):
+    try:
+        return
+    except:
+        return
