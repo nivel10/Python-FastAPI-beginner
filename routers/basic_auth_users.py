a9e12cc5824b004f8af546c3246593c9da9cc88e
T.S.U. - Ing. Carlos H. <carlos.e.herrera.j@gmail.com>
2025-08-05 13:49:05 -0400

    basic auth users
    basic auth users

--- routers/basic_auth_users.py
+++ routers/basic_auth_users.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends, HTTPException
+from fastapi import FastAPI, Depends, HTTPException, status
 from pydantic import BaseModel
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
@@ -37,22 +37,32 @@ users_db: object = {
 
 def search_user(user_name: str):
     if user_name in users_db:
-        return UserDB(users_db[user_name])
+        # # return UserDB(users_db[user_name])
+        # return UserDB(**users_db[user_name])
+        return users_db[user_name]
 
-async def current_user():
-    pass
+async def current_user(token: str = Depends(oauth2)):
+    user: UserDB = search_user(user_name=token)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, 
+            detail='Authentication credentials invalid',
+            headers={'www-Authenticate': 'Bearer'}
+        )
+    
+    return user
 
 @app.post('/login')
 async def login(form: OAuth2PasswordRequestForm = Depends()):
     user_db = users_db.get(form.username)
     if not user_db:
-        raise HTTPException(status_code=400, detail='user is not valid')
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='user is not valid')
+
+    user: UserDB = search_user(user_name=form.username)
+    if not user['password'] == form.password:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='user or password incorrect')
     
-    user: UserDB = search_user(form.username)
-    if not user.password == form.password:
-        raise HTTPException(status_code=400, detail='user or password incorrect')
-    
-    return {'access_token': user.user_name, 'token_type': 'bearer',}
+    return {'access_token': user['user_name'], 'token_type': 'bearer',}
 
 @app.get('/users/me')
 async def users_me(user: User = Depends(current_user)):
