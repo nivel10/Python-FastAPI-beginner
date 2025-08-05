57f4962b438266895cf89abc69f06efa90ac88bf
T.S.U. - Ing. Carlos H. <carlos.e.herrera.j@gmail.com>
2025-08-05 18:13:43 -0400

    jwt
    jwt

--- routers/jwt_auth_users.py
+++ routers/jwt_auth_users.py
@@ -2,13 +2,18 @@ from fastapi import FastAPI, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from pydantic import BaseModel
 from jose import jwt
+from passlib.context import CryptContext
+from datetime import datetime, timedelta
 
-ALGORITHM = 'HS256'
+ACCESS_TOKEN_ALGORITHM = 'HS256'
+ACCESS_TOKEN_DURATION = 1
+ACCESS_TOTEN_KEY = '35852669a66715cbe7145ecb9d502e8b9cc86a0c400445fc4ff5bc39e969e27b'
 
 app = FastAPI()
 
-oauth2 = OAuth2PasswordBearer(tokenUrl='/login')
-
+oauth2 = OAuth2PasswordBearer(tokenUrl='login')
+crypt = CryptContext(schemes=['bcrypt'])
+ 
 class User(BaseModel):
     user_name: str
     firts_name: str
@@ -26,7 +31,7 @@ users_db: object = {
         'last_name': 'Herrera',
         'email': 'nikole.herrera@gmail.com',
         'disabled': False,
-        'password': '123456'
+        'password': '$2a$12$4rzUI.K8mERTsagSEFdFT.fgOoe09h1ckK0qEqlLyMeFdtWR7mSiS'
     },
     'carlos': {
         'user_name': 'carlos',
@@ -34,7 +39,7 @@ users_db: object = {
         'last_name': 'Herrera',
         'email': 'carlos.herrera@gmail.com',
         'disabled': True,
-        'password': '123456'
+        'password': '$2a$12$FUDw2dx1xtaptGulMhqlPejkhmUBKdIyYMLqzIhaLBmRenm16Rxfe'
     },
 }
 
@@ -52,7 +57,19 @@ async def login(form: OAuth2PasswordRequestForm = Depends()):
         )
     
     user: UserDB = search_user_db(user_name=form.username)
-    if not user['password'] == form.password:
+    user_password_verify = crypt.verify(form.password, user['password'])
+    if not user_password_verify:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='user or password incorrect')
     
-    return {'access_token': user['user_name'], 'token_type': 'bearer',}
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_DURATION)
+    access_token_payloads: object = {
+        'sub': user['user_name'],
+        'exp': expire,
+    }
+    access_token = jwt.encode(
+        claims=access_token_payloads,
+        key=ACCESS_TOTEN_KEY,
+        algorithm=ACCESS_TOKEN_ALGORITHM
+    )
+
+    return {'access_token': access_token, 'token_type': 'bearer',}
